0d172a1facee65f5bfd980dc4f1b1c3d8d8e9fb7
aus <austinrobertson@gmail.com>
2018-11-20 18:41:57 -0600

    fix indention

--- bin/pfatt.sh
+++ bin/pfatt.sh
@@ -28,12 +28,12 @@ getTimestamp(){
 
     echo "$(getTimestamp) building netgraph nodes..."
 
-    echo -n "$(getTimestamp)  creating ng_one2many... "
+    echo -n "$(getTimestamp)   creating ng_one2many... "
     /usr/sbin/ngctl mkpeer $ONT_IF: one2many lower one
     /usr/sbin/ngctl name $ONT_IF:lower o2m
     echo "OK!"
 
-    echo -n "$(getTimestamp)  creating vlan node and interface... "
+    echo -n "$(getTimestamp)   creating vlan node and interface... "
     /usr/sbin/ngctl mkpeer o2m: vlan many0 downstream
     /usr/sbin/ngctl name o2m:many0 vlan0
     /usr/sbin/ngctl mkpeer vlan0: eiface vlan0 ether
@@ -42,41 +42,41 @@ getTimestamp(){
     /usr/sbin/ngctl msg ngeth0: set $RG_ETHER_ADDR
     echo "OK!"
 
-    echo -n "$(getTimestamp)  defining etf for $ONT_IF (ONT)... "
+    echo -n "$(getTimestamp)   defining etf for $ONT_IF (ONT)... "
     /usr/sbin/ngctl mkpeer o2m: etf many1 downstream
     /usr/sbin/ngctl name o2m:many1 waneapfilter
     /usr/sbin/ngctl connect waneapfilter: $ONT_IF: nomatch upper
     echo "OK!"
 
-    echo -n "$(getTimestamp)  defining etf for $RG_IF (RG)... "
+    echo -n "$(getTimestamp)   defining etf for $RG_IF (RG)... "
     /usr/sbin/ngctl mkpeer $RG_IF: etf lower downstream
     /usr/sbin/ngctl name $RG_IF:lower laneapfilter
     /usr/sbin/ngctl connect laneapfilter: $RG_IF: nomatch upper
     echo "OK!"
 
-    echo -n "$(getTimestamp)  bridging etf for $ONT_IF <-> $RG_IF... "
+    echo -n "$(getTimestamp)   bridging etf for $ONT_IF <-> $RG_IF... "
     /usr/sbin/ngctl connect waneapfilter: laneapfilter: eapout eapout
     echo "OK!"
 
-    echo -n "$(getTimestamp)  defining filters for EAP traffic... "
+    echo -n "$(getTimestamp)   defining filters for EAP traffic... "
     /usr/sbin/ngctl msg waneapfilter: 'setfilter { matchhook="eapout" ethertype=0x888e }'
     /usr/sbin/ngctl msg laneapfilter: 'setfilter { matchhook="eapout" ethertype=0x888e }'
     echo "OK!"
 
-    echo -n "$(getTimestamp)  enabling one2many links... "
+    echo -n "$(getTimestamp)   enabling one2many links... "
     /usr/sbin/ngctl msg o2m: setconfig "{ xmitAlg=2 failAlg=1 enabledLinks=[ 1 1 ] }"
     echo "OK!"
 
-    echo -n "$(getTimestamp)  removing waneapfilter:nomatch hook... "
+    echo -n "$(getTimestamp)   removing waneapfilter:nomatch hook... "
     /usr/sbin/ngctl rmhook waneapfilter: nomatch
     echo "OK!"
 
     echo "$(getTimestamp) enabling interfaces..."
-    echo -n "  $RG_IF ... "
+    echo -n "$(getTimestamp)   $RG_IF ... "
     /sbin/ifconfig $RG_IF up
     echo "OK!"
 
-    echo -n "  $ONT_IF ... "
+    echo -n "$(getTimestamp)   $ONT_IF ... "
     /sbin/ifconfig $ONT_IF up
     echo "OK!"
 
